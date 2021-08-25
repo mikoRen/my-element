683310fa04b0960eafaa5c7843af88ba7bae110b
miko Ren <miko@mikodeMacBook-Pro.local>
2021-08-25 11:26:08 +0800

    Button Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, {ButtonType, ButtonSize} from './components/Button/button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button className="custom">Default</Button>
+        <Button onClick={(e) => {e.preventDefault(); alert(123) }} btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small} >Small Danger</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com/" target = "_blank" >baidu link</Button>
+        <br />
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large} disabled>Large Primary</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small} disabled >Small Danger</Button>
+        <Button btnType={ButtonType.Link} href={"https://www.baidu.com/"} disabled  >baidu link</Button>
       </header>
     </div>
   );
